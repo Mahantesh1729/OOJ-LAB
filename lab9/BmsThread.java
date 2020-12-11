5e90282b17abe32cf18525a0b8337e7db81cca14
Mahantesh1729 <67777625+Mahantesh1729@users.noreply.github.com>
2020-12-11 11:59:47 +0530

    Add files via upload

---
+++ lab9/BmsThread.java
@@ -0,0 +1,71 @@
+class BmsThread
+{
+	public static void main(String[] args)
+	{
+		Thread1 a = new Thread1("BMS COLLEGE OF ENGINEERING");
+		Thread2 b = new Thread2("CSE");
+		try
+		{
+			a.t.join();
+			b.t.join();
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Main thread Interuppted");
+		}
+	}
+}
+
+class Thread1 implements Runnable
+{
+	String name;
+	Thread t;
+	Thread1(String threadname)
+	{
+		name = threadname;
+		t = new Thread(this, name);
+		t.start();
+	}
+	
+	public void run()
+	{
+		try
+		{
+			for(int i = 5; i > 0; i--)
+			{
+				System.out.println(name);
+				Thread.sleep(10000);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Interuupted in BMS");
+		}
+	}
+}
+class Thread2 implements Runnable
+{
+	String name;
+	Thread t;
+	Thread2(String threadname)
+	{
+		name = threadname;
+		t = new Thread(this, name);
+		t.start();
+	}
+	public void run()
+	{
+		try
+		{
+			for(int i = 5; i > 0; i--)
+			{
+				System.out.println(name);
+				Thread.sleep(2000);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Interuupted in CSE");
+		}
+	}
+}
